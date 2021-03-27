3d03f15b20e81e805946375e95e85143463e5c64
袁震阳 <yuanzhenyang@yuanzhenyangdeMacBook-Pro.local>
2021-03-27 23:14:05 +0100

    well commented

--- sem_1d.py
+++ sem_1d.py
@@ -10,6 +10,7 @@ from math import pi
 import math
 from numpy.linalg import norm
 from scipy.linalg import solve_banded
+from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 params = {'legend.fontsize': 15,
@@ -28,12 +29,12 @@ pylab.rcParams.update(params)
 # main function
 def main():
 
-
+    #Initialize varibles
     (nel,L,x,ord,M_g,L_g,D_g,h) = Initialization()
+    #calculate GLL points, weights and Legendre polynomial at GLL points
     (xe,w,L_p,dL_p) = Gauss_Legendre_weight(ord)
 
-
-    x_total = np.zeros((ord-1)*nel+1)
+    x_total = np.zeros((ord-1)*nel+1)           #total points in the domain
     for i in range(nel):
         a = x[i]
         b = x[i+1]
@@ -42,35 +43,40 @@ def main():
     x_total[len(x_total)-1] = x[len(x)-1]
 
 
-
-
+    #trial functions and dirivation of trial functions
     phi,dphi = construct_weight_function(xe,ord,L_p,dL_p)
+    #build local matrices
     (M_e,L_e,D_e) = local_matrices(ord,w,phi,dphi,h)
-    A,R = assembly_matrix(xe,nel)
+    #global assemble matrix and boundary condition matrix
+    #note: bc = 0 dirichilet bc = 1 neumann bc = 2 Periodic
+    bc = 2
+    A,R = assembly_matrix(xe,nel,bc)
+    #assemble global matrices
     M_g,L_g,D_g = global_matrix(M_e,M_g,L_e,L_g,D_e,D_g,nel,A,R)
 
 
-    #print(dphi)
-    #print(D_e,L_e)
-    #
-    #
+    #solver
     u,uu,x_total = solver(x_total,L,M_g,L_g,D_g,xe,R)
+    #plot function
     plot(x_total,u,uu)
 
 
 
 
 def Initialization():
-    nel = 5 #element number
-    ord = 25 #number of the Gauss_Lobatto_Legendre points
-    L = 1 #domain size
-    x = np.linspace(0,L,nel+1)   #grid points
+    nel = 6                                     #element number
+    ord = 15                                    #number of the Gauss_Lobatto_Legendre points
+    L = 1                                       #domain size
+    x = np.linspace(0,L,nel+1)                  #grid points
 
-    h = L/nel
+    h = L/nel                                   #element size, even here but can be changed to uneven(more pratical)
 
-    M_g = np.zeros(( (ord-1)*nel,(ord-1)*nel ))
-    L_g = np.zeros(( (ord-1)*nel,(ord-1)*nel ))
-    D_g = np.zeros(( (ord-1)*nel,(ord-1)*nel ))
+    M_g = csr_matrix(( (ord-1)*nel,(ord-1)*nel ))   #using sparse matrix here for the reason that those matrices are diagonal dominated
+    L_g = csr_matrix(( (ord-1)*nel,(ord-1)*nel ))
+    D_g = csr_matrix(( (ord-1)*nel,(ord-1)*nel ))
+
+
+
 
     return nel,L,x,ord,M_g,L_g,D_g,h
 
@@ -117,14 +123,14 @@ def Gauss_Legendre_weight(ord):
         P[1,:] = x
 
         for k in range(1,N):
-            P[k+1,:] = ( (2 * k + 1) * x * P[k,:] - k*P[k-1,:] ) / (k + 1);
+            P[k+1,:] = ( (2 * k + 1) * x * P[k,:] - k*P[k-1,:] ) / (k + 1);   #legendre poly
 
 
         x = xold - ( x * P[N1-1,:] - P[N-1,:] ) / ( N1 * P[N1-1,:] )
 
     dP[0,:] = 0
     for k in range(N):
-        dP[k+1,:] = k*P[k,:] + x*dP[k,:]
+        dP[k+1,:] = k*P[k,:] + x*dP[k,:]    #derivatives of legendre poly, not used in this case
 
     w = 2 / (N * N1 * P[N1-1,:]**2 );
 
@@ -170,7 +176,7 @@ def build_lagrange_poly(xe,i,j,diff,L_p):
         #         sum += u / l / (xe[i] - xe[n])
         # return sum
         N = len(xe) - 1
-
+        # this code can be found in DEVILLE's book, chapter 2, sem Section
         if i == 0 and j == 0:
             return -(N+1)*N/4
         elif i == N and j == N:
@@ -192,10 +198,7 @@ def local_matrices(ord,w,phi,dphi,h):
     for i in range(ord):
         for j in range(ord):
             for k in range(len(w)):
-                # M_e[i,j] += phi[i,k]*phi[j,k]*w[k]*h
-                # L_e[i,j] += dphi[i,k]*dphi[j,k]*w[k]/h
-                # D_e[i,j] += phi[i,k]*dphi[j,k]*w[k]
-                M_e[i,j] += phi[k,i]*phi[k,j]*w[k]*h
+                M_e[i,j] += phi[k,i]*phi[k,j]*w[k]*h   #since Je = h, dxe/dx = 1/h, some calculation is predone in the code
                 L_e[i,j] += dphi[k,i]*dphi[k,j]*w[k]/h
                 D_e[i,j] += phi[k,i]*dphi[k,j]*w[k]
 
@@ -203,46 +206,48 @@ def local_matrices(ord,w,phi,dphi,h):
     return M_e,L_e,D_e
 
 
-def assembly_matrix(xe,nel):
-#     #print(np.kron(np.ones((nel,1)),np.eye(xe+2)))  #Kronecker product of two arrays.
+def assembly_matrix(xe,nel,bc):
+    #print(np.kron(np.ones((nel,1)),np.eye(xe+2)))  #Kronecker product of two arrays.
 
-    #element continuity
+    #element continuity, not very efficient way to contruct A
     a = np.eye(nel)
     b = np.zeros((len(xe),len(xe)-1))
     b[0:len(xe)-1,0:len(xe)-1] = np.eye(len(xe)-1)
     b[len(xe)-1,len(xe)-2] = 1
+
     A = np.zeros((len(xe)*nel,len(xe)*nel-nel+1))
-    #print(len(xe))
 
     c = np.kron(a,b)
     c = np.delete(c,len(xe)*nel-2,0)  #delete the last row to contruct A
+
     A[0,0] = 1
-
     A[1:len(xe)*nel,1:len(xe)*nel-nel+1] = c
-    #print(A)
 
 
-    #homogenous dirichilet boundary conditions
+
+    #boundary conditions
     N = len(xe)*nel-nel+1
-    #if both bc are dirichilet
-    # R = np.zeros(( N-2,N ))
-    # R[:,1:R.shape[1]-1] = np.eye(R.shape[1]-2)
+    #if both bc are homogenous dirichilet
+    if bc == 0:
+        R = np.zeros(( N-2,N ))
+        R[:,1:R.shape[1]-1] = np.eye(R.shape[1]-2)
     #if bc on the right is neumann
-    # R = np.zeros(( N-1,N ))
-    # R[:,1:R.shape[1]] = np.eye(R.shape[1]-1)
+    if bc == 1:
+        R = np.zeros(( N-1,N ))
+        R[:,1:R.shape[1]] = np.eye(R.shape[1]-1)
     # if Periodic bc
-    R = np.zeros(( N-1,N ))
-    R[:,0:R.shape[1]-1] = np.eye(N-1)
-    A[A.shape[0]-1,0] = 1
-    A[A.shape[0]-1,A.shape[1]-1] = 0
-    # A = A[:,0:A.shape[1]-1]
+    if bc == 2:
+        R = np.zeros(( N-1,N ))
+        R[:,0:R.shape[1]-1] = np.eye(N-1)
+        A[A.shape[0]-1,0] = 1
+        A[A.shape[0]-1,A.shape[1]-1] = 0
 
 
     return A,R
 
 
 def global_matrix(M_e,M_g,L_e,L_g,D_e,D_g,nel,A,R):
-    M_g = np.matmul(np.matmul(np.transpose(A) , np.kron(np.eye(nel),M_e)) , A)
+    M_g = np.matmul(np.matmul(np.transpose(A) , np.kron(np.eye(nel),M_e)) , A)  #put each element into right place
     L_g = np.matmul(np.matmul(np.transpose(A) , np.kron(np.eye(nel),L_e)) , A)
     D_g = np.matmul(np.matmul(np.transpose(A) , np.kron(np.eye(nel),D_e)) , A)
 
@@ -256,35 +261,31 @@ def global_matrix(M_e,M_g,L_e,L_g,D_e,D_g,nel,A,R):
 
 def solver_pre(M_g,L_g,D_g,dt,u,xe):
 
-    rhs = M_g - 1 * D_g * dt - 0.1 * L_g * dt
+    rhs = M_g - 1 * D_g * dt - 0.1 * L_g * dt               #advection speed = 1, nv = 0.1
 
 
 
-    #lhs = reshape_lhs(M_g,xe)   #useful when doing LU factorization
-    #u = solve_banded((len(xe)-1, len(xe)-1), lhs, rhs)   #solve_banded(l_and_u, A, b), system Ax = b
+    #lhs = reshape_lhs(M_g,xe)                              #useful when doing LU factorization
+    #u = solve_banded((len(xe)-1, len(xe)-1), lhs, rhs)     #solve_banded(l_and_u, A, b), system Ax = b
     lhs = np.zeros((M_g.shape[0],M_g.shape[0]))
     for i in range(M_g.shape[0]):
-        lhs[i][i] = 1/M_g[i][i]
+        lhs[i][i] = 1/M_g[i][i]                             #since M_e is diagonal, LU is not needed
 
-    coeffMat = np.matmul(lhs,rhs)
-
-
-
+    coeffMat = np.matmul(lhs,rhs)                           #pre-calculate coeffcient Matrix
 
     return coeffMat
 
 
 def solver(x_total,L,M_g,L_g,D_g,xe,R):
     u_ini = np.transpose(np.sin(x_total*4*3.14/L))
-    u = np.matmul(R,u_ini)
-    x_total = np.matmul(R,x_total)
+    u = np.matmul(R,u_ini)                                  #apply boundary condition to u
+    x_total = np.matmul(R,x_total)                          #also can be done to x to save effort, mathematically, it is right, phically is wrong
     uu = u
 
-    tfinal = 0.1
-    CFL = 0.0001
+    tfinal = 0.1                                            #final time
+    CFL = 0.0001                                            #related to stability analysis, this number can be larger
     c = 1
     dt = CFL*(x_total[2]-x_total[1])/c
-    #dt = 0.000001
     step = math.floor(tfinal/dt)
     # step = 1
 
@@ -293,6 +294,7 @@ def solver(x_total,L,M_g,L_g,D_g,xe,R):
 
     for t in range(step):
         u = np.matmul(coeffMat,u)
+        print('physical time = '+str(dt+t*dt))
 
     return u,uu,x_total
 
@@ -308,7 +310,7 @@ def plot(x_total,u,uu):
 
 
 
-def reshape_lhs(lhs,xe):
+def reshape_lhs(lhs,xe):    #for Lu factorization use, not used here
     lhs_temp = np.zeros((len(xe)*2-1,lhs.shape[0]))
 
     for i in range(lhs.shape[0]):
@@ -321,7 +323,6 @@ def reshape_lhs(lhs,xe):
                 lhs_temp[len(xe)-1-j,i+j] = lhs[i,i+j]
                 lhs_temp[len(xe)-1+j,i] = lhs[i+j,i]
 
-#    print(rhs_temp)
     return lhs_temp
 
 
